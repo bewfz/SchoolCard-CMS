e5748943ea273a70e576631139efffc168ffb428
J <114867070+lansley@users.noreply.github.com>
2023-12-23 09:47:32 +0000

    Add public routes to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRoutes = require('./auth');
 const cardRoutes = require('./card');
+const publicRoutes = require('./public');
 
 router.use('/auth', authRoutes);
 router.use('/card', cardRoutes);
+router.use('/public', publicRoutes);
 
 module.exports = router;
