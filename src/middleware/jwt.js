f1577a040e08af611abd40fd0044bcd59d49132b
J <114867070+lansley@users.noreply.github.com>
2023-12-23 07:34:01 +0000

    Update JWT secret in middleware

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
 
 module.exports = function(req, res, next) {
     const token = req.header('Authorization');
     if (!token) return res.status(401).send('Access Denied');
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (err) {
+        //debug
+        console.log(err);
         res.status(400).send('Invalid Token');
     }
 };
