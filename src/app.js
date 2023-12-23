a8198d6b9bcb5775c02d6cd54d13da8439a8e31b
J <114867070+lansley@users.noreply.github.com>
2023-12-23 07:23:26 +0000

    Add express server setup and error handling middleware

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwtMiddleware = require('./middleware/jwt');
+const routes = require('./routes');
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000`);
+});
+// 使用body-parser处理json数据
+app.use(bodyParser.json());
+// 设置路由
+app.use('/api', routes);
+
+// 错误处理中间件
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
+
+module.exports = app;
