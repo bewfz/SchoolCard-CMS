a6b21b418060810c72907726b9f6a27f56dd5e3e
J <114867070+lansley@users.noreply.github.com>
2023-12-23 09:47:26 +0000

    Add debug statement to log user's username

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -9,6 +9,8 @@ module.exports = function(req, res, next) {
     try {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
+        //debug
+        //console.log(req.user.username);
         next();
     } catch (err) {
         //debug
